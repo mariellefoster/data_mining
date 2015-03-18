5f15ccc6096cf485011e6d25479b87811a0dc885
Marielle Foster <marielleefoster@gmail.com>
2015-03-18 18:21:50 -0500

    Final

--- adaboost.py
+++ adaboost.py
@@ -2,11 +2,9 @@
 #By Marielle Foster and Nick Petru
 from numpy import *
 import math
-# import numpy
 
 def read_in_data_titanic():
 	fl = open("titanic.dat")
-
 	data_points = []
 	class_labels = []
 	for line in fl:
@@ -22,12 +20,32 @@ def read_in_data_titanic():
 		class_labels.append(y)
 	return data_points, class_labels
 
+def read_in_data_CG():
+	fl = open("letterCG.data")
+	data_points = []
+	class_labels = []
+	for line in fl:
+		# attributes distinguishing C's and G's
+		line = line.split()
+		for i in range(len(line)):
+			if line[i] == 'C':
+				line[i] = -1
+			if line[i] == 'G':
+				line[i] = 1
+			line[i] = int(line[i])
+		x = line[1:]
+		y = line[0]
+		data_points.append(x)
+		class_labels.append(y)
+	return data_points, class_labels
+
+#takes in threshold and makes predictions for that stump, returns predictions
 def classify(data_points, attr, threshhold, ineq):
 	predictions = []
 	num_items, num_attr = shape(data_points) #items in this case can be number of data_points
 	for i in range(num_items):
 		predictions.append(1)
-	#predictions = matrix(predictions)
+
 	for i in range(num_items):
 		pass
 		if ineq == 0: # less than
@@ -36,13 +54,11 @@ def classify(data_points, attr, threshhold, ineq):
 		if ineq == 1:
 			if data_points[i][:,attr] >= threshhold:
 				predictions[i] = -1
-	# print predictions
 	return predictions
 
-#takes in a weight vector D, 
+#takes in a weight vector D, our data, the labels, the error for this time and the class estimate
 def build_decision_stump(D, data_points, class_labels, error, class_estimate):
-	# 	Set the min_error to +infinity
-	#error = inf
+	# Min error previously set to infinity
 	num_items, num_attr = shape(data_points)	
 
 	best_stump = {}
@@ -77,8 +93,7 @@ def build_decision_stump(D, data_points, class_labels, error, class_estimate):
  					best_stump["threshhold"] = threshhold
  					best_stump["inequality"] = ineq
 
-				# 	If the error is less than minError: set this stump as the best stump
-	#	 Return the best stump
+	# Return the best stump
 	return best_stump, error, class_estimate
 
 # http://www.cs.princeton.edu/~schapire/papers/explaining-adaboost.pdf Page 2 saved us
@@ -86,7 +101,7 @@ def calc_D(weak_stump, class_labels, class_estimate, D):
 	neg_alpha = -1 * weak_stump["alpha"]
 	D = matrix(D)
 
-	# exponent = [neg_alpha for i in range(len(class_labels))]
+	#find the exponent vector to multiply D by. This took ages to understand.
 	exponent = exp(multiply(neg_alpha* matrix(class_labels).T, matrix(class_estimate).T))
 	new_D = multiply(D, exponent)
 	return new_D
@@ -95,23 +110,25 @@ def train_decision_stumps(data_points, class_labels, num_iterations):
 	num_items, num_attr = shape(data_points)
 	best_stump_arr = []
 
+	# make the weight value vector D
 	D = []
 	for i in range(num_items):
 		D.append([1/float(num_items)])
 
 	total_estimate = [0 for i in range(num_items)]
 	total_estimate = matrix(total_estimate)
-	# print D_t
+
 	# For each iteration:
 	for i in range(num_iterations):
 		error = inf
 		class_estimate = []
 
-		# 	Find the best stump using build_decision_stump()
+		# Find the best stump using build_decision_stump()
 		# assume the stump is a dictionary, containing the dimension value, 
 		# the threshhold value and then finally add the alpha value
 		weak_stump, error, class_estimate = build_decision_stump(D, data_points, class_labels, error, class_estimate)
-		# 	Calculate alpha
+		
+		# Calculate alpha
 		error = float(error)
 		if error > 0.0:
 			alpha = math.log((1-error)/error)/float(2)
@@ -119,23 +136,21 @@ def train_decision_stumps(data_points, class_labels, num_iterations):
 			alpha = 0.0
 		weak_stump["alpha"] = alpha
 
-		# 	Add the best stump to the stump array
+		# Add the best stump to the stump array
 		best_stump_arr.append(weak_stump)	
-		# 	Calculate the new weight vector D
+		# Calculate the new weight vector D
 		D = calc_D(weak_stump, class_labels, class_estimate, D)
-		# 	Update the aggregate class estimate
+		# Update the aggregate class estimate
 		total_estimate = total_estimate + alpha*matrix(class_estimate)
-		#*******************
-		#total_errors = sum(total_estimate)/float(num_items)
 
 		total_errors = 0
-		total_estimate = total_estimate.T
-		
+		total_estimate = total_estimate.T		
 		# Add up all the times the sign error (and therefore the likely prediction)
 		# Is off, and then divide by the total number of datapoints.
 		for k in range(len(class_labels)):
 			if sign(class_labels[k]) != sign(total_estimate[k]):
 				total_errors += 1
+
 		total_estimate = total_estimate.T
 
 		error_rate = total_errors/float(num_items)
@@ -156,5 +171,13 @@ def main():
 	data_points = matrix(data_points)
 	
 	train_decision_stumps(data_points, class_labels, 10)
+
+	print
+	print "Adaboost on C, G letter identification"
+	data_points, class_labels = read_in_data_CG()
+
+	data_points = matrix(data_points)
 	
+	train_decision_stumps(data_points, class_labels, 10)
+
 main()
